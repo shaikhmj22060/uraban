0baf374ecaf45759fbf0dba710f7b2f454fb0cf6
shaikhmj22060 <shaikhm.ja.z@gmail.com>
2025-03-12 15:17:44 +0530

    "service page added"

--- urbanclap/clone/views.py
+++ urbanclap/clone/views.py
@@ -37,3 +37,6 @@ def login_user(request):
 def logout_user(request):
     logout(request)
     return redirect('login_user')
+@login_required
+def service(request):
+    return render(request,'clone/service.html')
